e1b5801cf51418035903efa04699f29c30a9bfb9
leiger <leiger@bupt.edu.cn>
2018-05-06 21:14:44 -0400

    v1.0

---
+++ depth_first_search/tree.py
@@ -0,0 +1,33 @@
+# 100. Same Tree
+# recursion way(DFS)
+# no recursion: stack(DFS)  pop()  append()
+
+# 101. Symmetric Tree
+# recursion (DFS), we need to form a new function, 
+# cause we need two input
+# no recursion(the same as #100)
+
+# 110. Balanced Binary Tree
+# DFS
+# the height of each subtree should be record and compared
+
+# 111. Minumum Depth of Binary Tree
+# Both empty; one empty; no empty
+
+# 112. Maxmum Depth of Binary Tree
+
+
+# 105. Construct Binary Tree from Preorder and Inorder Traversal
+# preorder; inorder; postorder
+
+# 106. Construct Binary Tree from inorder and postorder Traversal
+
+# 199. Binary Tree Right Side View
+# level(depth) and list
+
+# 261. Graph Valid Tree
+# 边数和节点数的关系；是否有可能形成环
+
+# 114. Flatten Binary Tree to Linked List
+# reversed preorder
+# 还可以用栈
